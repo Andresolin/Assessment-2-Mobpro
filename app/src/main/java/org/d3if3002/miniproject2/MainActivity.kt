c928eaa2ae6db9a549fb75d24d4f33d7bffc5a56
Andre Permana Solin <permanasolin@gmail.com>
2024-05-10 18:31:35 +0700

    Menambahkan Navigation component

--- app/src/main/java/org/d3if3002/miniproject2/MainActivity.kt
+++ app/src/main/java/org/d3if3002/miniproject2/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import org.d3if3002.miniproject2.navigation.SetupNavGraph
 import org.d3if3002.miniproject2.ui.Screen.MainScreen
 import org.d3if3002.miniproject2.ui.theme.MiniProject2Theme
 
@@ -23,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainScreen()
+                    SetupNavGraph()
                 }
             }
         }
---
+++ app/src/main/java/org/d3if3002/miniproject2/navigation/NavGraph.kt
@@ -0,0 +1,22 @@
+package org.d3if3002.miniproject2.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import org.d3if3002.miniproject2.ui.Screen.MainScreen
+
+
+
+@Composable
+fun SetupNavGraph(navController: NavHostController = rememberNavController()) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ) {
+        composable(route = Screen.Home.route) {
+            MainScreen()
+        }
+    }
+}
